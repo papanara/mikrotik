8dc21c31e87be1ae8464ccdaf260e4d5851f0849
Agan Nain <naramedia.digital@icloud.com>
2023-04-04 22:02:55 +0800

    Update mikbot.py

--- mikbot.py
+++ mikbot.py
@@ -12,8 +12,8 @@ endpoint = 'https://ip_mikrotik/rest'
 bot_token = 'token_bot_telegram_disini'
 bot       = telebot.Telebot(bot_token)
 
-#check user active
-@bot.message_handler(commands=['user-active']) #using this command on telegram chat
+#check user active hotspot
+@bot.message_handler(commands=['hotspot-active'])
 def active_user(message):
         count = 0
         user_active = requests.get(endpoint+'/ip/hotspot/active', auth=login, verify=False)
@@ -21,5 +21,15 @@ def active_user(message):
                 count+=1
         bot.reply_to(message, '<b>🙎 User Aktif Saat Ini : '+str(count)+'</b>', parse_mode='html')
         print('log: active_user function')
+        
+#check pppoe active
+@bot.message_handler(commands=['pppoe-active'])
+def active_pppoe(message):
+        count = 0
+        pppoe_active = requests.get(endpoint+'/ppp/active' auth=login, verify=False)
+        for pa in pppoe_active.json():
+                count+=1
+        bot.reply_to(message, '<b>PPPoE Aktif Saat Ini : '+str(count)+'</b>', parse_mode='html')
+        print('log: pppoe_active function')
 
 bot.infinity_polling()
